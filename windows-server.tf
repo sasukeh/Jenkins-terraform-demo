c3634a22fd2b30d01b11b481b6d02e6ffc9d140e
Kyohei Moriyama a.k.a sasukeh <flogofrein@gmail.com>
2018-04-03 18:18:47 +0900

    init

--- windows-server.tf
+++ windows-server.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_interface" "test" {
 
 resource "azurerm_virtual_machine" "virtual_machine" {
   name                  = "test"
-  location              = "${var.location}"
+  location              = "${azurerm_resource_group.test.location}"
   resource_group_name   = "test"
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
   vm_size               = "Standard_DS1_v2"
@@ -83,7 +83,7 @@ resource "azurerm_virtual_machine" "virtual_machine" {
 
 resource "azurerm_virtual_machine_extension" "virtual_machine_extension" {
   name                 = "test"
-  location             = "${var.location}"
+  location              = "${azurerm_resource_group.test.location}"
   resource_group_name  = "test"
   virtual_machine_name = "${azurerm_virtual_machine.virtual_machine.name}"
   publisher            = "Microsoft.ManagedIdentity"
