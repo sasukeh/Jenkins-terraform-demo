0d979db194cbf7265920d9f33d250eefbd14b653
Kyohei Moriyama a.k.a sasukeh <flogofrein@gmail.com>
2018-04-03 18:15:59 +0900

    init

--- windows-server.tf
+++ windows-server.tf
@@ -1,9 +1,48 @@
 # Before executing this tf script, you should do following procedure.
-# 1. create resource_group
-# 2. az login
-# 3. az account list -o table
-# 4. az account set -s <subscription ID>
-#
+# 1. az login
+# 2. az account list -o table
+# 3. az account set -s <subscription ID>
+# 4. create resource_group
+# az group create -g test -l japaneast
+# az 
+
+# -----
+# Network Configuration
+# -----
+resource "azurerm_resource_group" "test" {
+  name     = "acceptanceTestResourceGroup1"
+  location = "Japan East"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "acceptanceTestVirtualNetwork1"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "testsubnet"
+  resource_group_name  = "${azurerm_resource_group.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "test" {
+  name                = "acceptanceTestNetworkInterface1"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.test.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+
+  tags {
+    environment = "staging"
+  }
+}
 
 resource "azurerm_virtual_machine" "virtual_machine" {
   name                  = "test"
